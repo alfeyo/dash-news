a38912d49e89a41c42ecead63deae936f47bf189
Daneil Sitali <reconvergence@hotmail.com>
2018-11-24 13:06:46 -0800

    forgot to push

--- src/App.js
+++ src/App.js
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=";
+const API_KEY = "da7b64a5971c488aa9804f6aa67164e4";
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      headlines : {
+        author: "",
+        content: "",
+        description: "",
+        title: "",
+        imageUrl: "",
+      },
+
+      articles: []
+    }
+  }
+  async componentDidMount(){
+    try {
+      const newsData = await fetch(API + API_KEY)
+      const newsDataJson = await newsData.json();
+      //const data = this.state.articles;
+      await this.setState({
+        articles: newsDataJson
+      })
+      //console.log(da);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
+    console.log(this.state.articles.articles);
     return (
       <div className="App">
         <header className="App-header">
@@ -24,5 +56,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
